fac301299a393e78b2afbd66832df8bb901d1105
ThiagoHFMC <thiago.frois.cunha@gmail.com>
2023-11-04 17:56:25 -0300

    feat: Adding an async wrapper for the controllers.
    The feature has the objective of making the controller asynchronous
    by using a wrapper. This is better because, instead of using promises
    with 'then,' we can now use 'await' and 'async' to structure the
    code more effectively.

--- controllers/cars.js
+++ controllers/cars.js
@@ -1,26 +1,45 @@
 const Car = require('../models/Car.js')
+const asyncWrapper = require('../middleware/async.js')
 
-const getCar = (req, res) => {
-    Car.findAll().then(e => res.json({"Sucess:":true, "Data":e}))
-}
+const getCar = asyncWrapper(async (req, res) => {
+    const cars = await Car.findAll()
+    res.status(200).json({ cars })
+})
 
-const getSingleCar = (req, res) => {
-    Car.findOne({ where : {id:req.params.id}}).then(e => res.json({"Sucess:":true, "Data":e}))
-}
+const getSingleCar = asyncWrapper(async (req, res, next) => {
+    const car = await Car.findOne({where : { id:req.params.id }})
+    if (!car) {
+        return next(Error("No task was found to be shown"))
+    }
 
-const createSingleCar = (req, res) => {
-    Car.build(req.body).save()
-    res.json({"Sucess":true, "Data":req.body})
-}
+    res.status(200).json({ car })
+})
 
-const updateSingleCar = (req, res) => {
-    Car.findOne({where : {id:req.params.id}}).then(e => {e.update(req.body); res.json({"Sucess":true, "Data":e})})
-}
+const createSingleCar = asyncWrapper(async (req, res) => {
+    const car = await Car.build(req.body).save()
+    res.status(201).json({ car })
+})
 
-const deleteSingleCar = (req, res) => {
-    Car.destroy({ where : {id:req.params.id}})
-    res.json({"Sucess":true, "Data":res.body})
-}
+const updateSingleCar = asyncWrapper(async (req, res, next) => {
+    const car = await Car.findOne({where : { id:req.params.id }})
+
+    if (!car) {
+        return next(Error("No task was found to be updated"))
+    }
+
+    car.update(req.body)
+    res.status(200).json({ car })
+})
+
+const deleteSingleCar = asyncWrapper(async (req, res, next) => {
+    const car = await Car.destroy({where : { id:req.params.id }})
+
+    if (!car) {
+        return next(new Error("No task was found to be deleted"))
+    }
+
+    res.status(200).json({ car })
+})
 
 module.exports = {
     getCar,
